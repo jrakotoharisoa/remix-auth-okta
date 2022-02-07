51ba44078cd464595bf2edb594172c20cd0099a3
Johann Rakotoharisoa <johann.rakotoharisoa@gmail.com>
2022-02-07 21:59:45 +0100

    ✨ Okta Auth code flow based on OAuth2Strategy

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,125 @@
 import { SessionStorage } from "@remix-run/server-runtime";
+import { AuthenticateOptions, StrategyVerifyCallback } from "remix-auth";
 import {
-  AuthenticateOptions,
-  Strategy,
-  StrategyVerifyCallback,
-} from "remix-auth";
+  OAuth2Profile,
+  OAuth2Strategy,
+  OAuth2StrategyOptions,
+  OAuth2StrategyVerifyParams,
+} from "remix-auth-oauth2";
 
-/**
- * This interface declares what configuration the strategy needs from the
- * developer to correctly work.
- */
-export interface MyStrategyOptions {
-  something: "You may need";
+export interface OktaProfile {
+  provider: string;
+  id: string;
+  displayName: string;
+  name: {
+    familyName: string;
+    givenName: string;
+    middleName: string;
+  };
+  email: string;
 }
 
-/**
- * This interface declares what the developer will receive from the strategy
- * to verify the user identity in their system.
- */
-export interface MyStrategyVerifyParams {
-  something: "Dev may need";
-}
+type OktaUserInfo = {
+  sub: string;
+  name: string;
+  preferred_username: string;
+  nickname: string;
+  given_name: string;
+  middle_name: string;
+  family_name: string;
+  profile: string;
+  zoneinfo: string;
+  locale: string;
+  updated_at: string;
+  email: string;
+  email_verified: boolean;
+};
 
-export class MyStrategy<User> extends Strategy<User, MyStrategyVerifyParams> {
-  name = "change-me";
+export type OktaStrategyOptions = Omit<
+  OAuth2StrategyOptions,
+  "authorizationURL" | "tokenURL" | "callbackURL"
+> & {
+  scope?: string;
+  issuer: string;
+} & (
+    | { flow: "Password"; callbackURL: never }
+    | { flow?: "Code"; callbackURL: string }
+  );
 
+export type OktaExtraParams = Record<string, string | number>;
+
+export class OktaStrategy<User> extends OAuth2Strategy<
+  User,
+  OktaProfile,
+  OktaExtraParams
+> {
+  name = "okta";
+  private userInfoURL: string;
+  private readonly scope: string;
+  private readonly flow: "Code" | "Password";
   constructor(
-    options: MyStrategyOptions,
-    verify: StrategyVerifyCallback<User, MyStrategyVerifyParams>
+    {
+      issuer,
+      scope = "openid profile email",
+      clientID,
+      clientSecret,
+      ...rest
+    }: OktaStrategyOptions,
+    verify: StrategyVerifyCallback<
+      User,
+      OAuth2StrategyVerifyParams<OAuth2Profile, OktaExtraParams>
+    >
   ) {
-    super(verify);
-    // do something with the options here
+    super(
+      {
+        authorizationURL: `${issuer}/v1/authorize`,
+        tokenURL: `${issuer}/v1/token`,
+        clientID,
+        clientSecret,
+        callbackURL: rest.flow === "Password" ? "" : rest.callbackURL,
+      },
+      verify
+    );
+    this.scope = scope;
+    this.userInfoURL = `${issuer}/v1/userinfo`;
+    this.flow = rest.flow ?? "Code";
   }
 
-  async authenticate(
+  authenticate(
     request: Request,
     sessionStorage: SessionStorage,
     options: AuthenticateOptions
   ): Promise<User> {
-    return await this.failure(
-      "Implement me!",
-      request,
-      sessionStorage,
-      options
-    );
-    // Uncomment me to do a success response
-    // this.success({} as User, request, sessionStorage, options);
+    if (this.flow === "Code") {
+      return super.authenticate(request, sessionStorage, options);
+    }
+
+    throw new Error("not implemented");
+  }
+
+  protected authorizationParams() {
+    return new URLSearchParams({
+      scope: this.scope,
+    });
+  }
+
+  protected async userProfile(accessToken: string): Promise<OktaProfile> {
+    const response = await fetch(this.userInfoURL, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const profile: OktaUserInfo = await response.json();
+    return {
+      provider: "okta",
+      id: profile.sub,
+      name: {
+        familyName: profile.family_name,
+        givenName: profile.given_name,
+        middleName: profile.middle_name,
+      },
+      displayName: profile.name ?? profile.preferred_username,
+      email: profile.email,
+    };
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,162 @@
 import { createCookieSessionStorage } from "@remix-run/server-runtime";
-import { MyStrategy } from "../src";
+import fetchMock, { enableFetchMocks } from "jest-fetch-mock";
+import { OAuth2StrategyVerifyParams } from "remix-auth-oauth2";
+import {
+  OktaExtraParams,
+  OktaProfile,
+  OktaStrategy,
+  OktaStrategyOptions,
+} from "../src";
 
-describe(MyStrategy, () => {
+enableFetchMocks();
+
+describe(OktaStrategy, () => {
   let verify = jest.fn();
   // You will probably need a sessionStorage to test the strategy.
   let sessionStorage = createCookieSessionStorage({
     cookie: { secrets: ["s3cr3t"] },
   });
 
+  let options: OktaStrategyOptions = Object.freeze({
+    flow: "Code",
+    issuer: "https://okta.issuer.come",
+    clientID: "CLIENT_ID",
+    clientSecret: "CLIENT_SECRET",
+    callbackURL: "https://mysite.com/okta/callback",
+  });
   beforeEach(() => {
     jest.resetAllMocks();
+    fetchMock.resetMocks();
   });
 
   test("should have the name of the strategy", () => {
-    let strategy = new MyStrategy({ something: "You may need" }, verify);
-    expect(strategy.name).toBe("change-me");
+    let strategy = new OktaStrategy(options, verify);
+    expect(strategy.name).toBe("okta");
   });
 
-  test.todo("Write more tests to check everything works as expected");
+  test("should have the scope `openid profile email` as default", async () => {
+    let strategy = new OktaStrategy(options, verify);
+
+    let request = new Request("https://mysite.com/okta/auth");
+
+    try {
+      await strategy.authenticate(request, sessionStorage, {
+        sessionKey: "user",
+      });
+    } catch (error) {
+      if (!(error instanceof Response)) throw error;
+      let location = error.headers.get("Location");
+
+      if (!location) throw new Error("No redirect header");
+
+      let redirectUrl = new URL(location);
+
+      expect(redirectUrl.searchParams.get("scope")).toBe(
+        "openid profile email"
+      );
+    }
+  });
+
+  test("should allow changing the scope", async () => {
+    let strategy = new OktaStrategy(
+      { ...options, scope: "custom scope" },
+      verify
+    );
+    let request = new Request("https://mysite.com/okta/auth");
+
+    try {
+      await strategy.authenticate(request, sessionStorage, {
+        sessionKey: "user",
+      });
+    } catch (error) {
+      if (!(error instanceof Response)) throw error;
+      let location = error.headers.get("Location");
+
+      if (!location) throw new Error("No redirect header");
+
+      let redirectUrl = new URL(location);
+
+      expect(redirectUrl.searchParams.get("scope")).toBe("custom scope");
+    }
+  });
+
+  test("should call verify with the access token, refresh token, extra params, user profile and context", async () => {
+    let strategy = new OktaStrategy(options, verify);
+
+    let session = await sessionStorage.getSession();
+    session.set("oauth2:state", "random-state");
+
+    let request = new Request(
+      `${options.callbackURL}?state=random-state&code=random-code`,
+      {
+        headers: { cookie: await sessionStorage.commitSession(session) },
+      }
+    );
+
+    fetchMock.once(
+      JSON.stringify({
+        access_token: "random-access-token",
+        refresh_token: "random-refresh-token",
+        id_token: "random.id.token",
+      })
+    );
+
+    fetchMock.once(
+      JSON.stringify({
+        sub: "id",
+        name: "Name",
+        preferred_username: "Preferred username",
+        nickname: "Nickname",
+        given_name: "Given name",
+        middle_name: "Middle name",
+        family_name: "Family name",
+        profile: "Profile",
+        zoneinfo: "fr",
+        locale: "fr",
+        updated_at: "date",
+        email: "example@email.com",
+        email_verified: true,
+      })
+    );
+
+    let context = { test: "it works" };
+
+    await strategy.authenticate(request, sessionStorage, {
+      sessionKey: "user",
+      context,
+    });
+
+    let [url, mockRequest] = fetchMock.mock.calls[0];
+    let body = mockRequest?.body as URLSearchParams;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let headers = mockRequest?.headers as any;
+
+    expect(url).toBe(`${options.issuer}/v1/token`);
+
+    expect(mockRequest?.method as string).toBe("POST");
+    expect(headers["Content-Type"]).toBe("application/x-www-form-urlencoded");
+
+    expect(body.get("client_id")).toBe(options.clientID);
+    expect(body.get("client_secret")).toBe(options.clientSecret);
+    expect(body.get("grant_type")).toBe("authorization_code");
+    expect(body.get("code")).toBe("random-code");
+
+    expect(verify).toHaveBeenLastCalledWith({
+      accessToken: "random-access-token",
+      refreshToken: "random-refresh-token",
+      extraParams: { id_token: "random.id.token" },
+      profile: {
+        provider: "okta",
+        id: "id",
+        displayName: "Name",
+        name: {
+          familyName: "Family name",
+          givenName: "Given name",
+          middleName: "Middle name",
+        },
+        email: "example@email.com",
+      },
+      context,
+    } as OAuth2StrategyVerifyParams<OktaProfile, OktaExtraParams>);
+  });
 });
